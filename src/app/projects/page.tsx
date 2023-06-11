b97c3a9fe537ea198a46254069f1518443eb9270
bimaaria <bima.aria.radjasa@gmail.com>
2023-06-11 09:33:43 +0700

    add title on projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,8 +2,8 @@ import ProjectCard from "./components/Card";
 
 export default function Projects() {
   return (
-    <section className="min-h-screen" id="projects">
-      <h1>Projects</h1>
+    <section className="flex flex-col px-4 py-10" id="projects">
+      <h1 className="text-xl font-bold">Projects</h1>
       <ProjectCard />
     </section>
   )
